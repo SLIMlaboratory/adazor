19555720820984d8007bc397742895b65bd71fec
Antoine Nasra <antoine.nasra@jrs.net>
2026-03-08 17:04:49 +0100

    adazor_library_test_

--- adazor/tests/test_imports.py
+++
@@ -1,9 +0,0 @@
-def test_public_api():
-    import adazor
-
-    assert hasattr(adazor, "GaussianMixture")
-    assert hasattr(adazor, "DriftingMixtureStream")
-    assert hasattr(adazor, "NoDriftDetector")
-    assert hasattr(adazor, "ThresholdDriftDetector")
-    assert hasattr(adazor, "ZTestDriftDetector")
-    assert hasattr(adazor, "HoeffdingTreeClassifier")
